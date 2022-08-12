aea6d439d2e7708c2c5304fbf3ce92673ef9b93c
J.Bae <johnny.bae00@outlook.com>
2022-08-11 20:40:51 -1000

    run rest api, run sln in 4.6

---
+++ ShoppingCart/EX-REST-Phase7/CartWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using MyApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for CartWindow.xaml
+    /// </summary>
+    public partial class CartWindow : Window
+    {
+        private ShoppingCart _cart;
+        public enum CartMode { Adding, Updating};
+        private CartMode _CurrentCartMode;
+
+        public CartWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadData(ShoppingCart cart, CartMode cartMode)
+        {
+            _cart = cart;
+            grdCart.DataContext = _cart;
+            lbCartItems.ItemsSource = _cart.Items;
+            _CurrentCartMode = cartMode;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (btnSave != null)
+                btnSave.IsEnabled = true;
+        }
+
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (_CurrentCartMode == CartMode.Updating)
+                await Shopping.UpdateCart(_cart);
+            else
+                await Shopping.AddCart(_cart);
+            Close();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var currentMaxItemNumber = _cart.Items.Count >0 ? _cart.Items.Max(i => i.ItemNumber) :0;
+            _cart.Items.Add(new CartItem() { ItemNumber = currentMaxItemNumber + 1 });
+            lbCartItems.Items.Refresh();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbCartItems.SelectedItem != null)
+            {
+                _cart.Items.Remove((CartItem)lbCartItems.SelectedValue);
+                lbCartItems.Items.Refresh();
+            }
+            else
+                MessageBox.Show("Please select which item to delete");
+        }
+    }
+}
---
+++ ShoppingCart/EX-REST-Phase7/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using MyApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<ShoppingCart> _carts;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        async void LoadData()
+        {
+            _carts = await Shopping.GetAllCarts();
+            lbCarts.ItemsSource = _carts;
+        }
+
+        private void btnUpdateCart_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbCarts.SelectedValue != null) {
+                CartWindow cartWindow = new CartWindow();
+                cartWindow.LoadData((ShoppingCart)lbCarts.SelectedValue, CartWindow.CartMode.Updating);
+                cartWindow.ShowDialog();
+            }
+            else
+                MessageBox.Show("Please select a cart");
+        }
+
+        private void btnAddCart_Click(object sender, RoutedEventArgs e)
+        {
+            CartWindow cartWindow = new CartWindow();
+            ShoppingCart newCart = new ShoppingCart()
+            {
+                CartNumber = _carts.Count > 0 ? _carts.Max(c => c.CartNumber) + 1 : 1,
+                CustomerName = "",
+                Items=new List<CartItem>()
+            };
+
+            cartWindow.LoadData(newCart, CartWindow.CartMode.Adding);
+            cartWindow.ShowDialog();
+            LoadData();
+            lbCarts.Items.Refresh();
+        }
+
+        private async void btnDeleteCart_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbCarts.SelectedItem != null)
+            {
+                await Shopping.DeleteCart(((ShoppingCart)lbCarts.SelectedValue).CartNumber);
+                LoadData();
+                lbCarts.Items.Refresh();
+            }
+            else
+                MessageBox.Show("Please select which cart to delete");
+        }
+    }
+}
